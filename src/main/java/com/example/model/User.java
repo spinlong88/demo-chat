d09fe211396967e879513b90f0e14eef038dd26f
Steel <273363837@qq.com>
2020-04-18 19:44:45 +0800

    first

---
+++ src/main/java/com/example/model/User.java
@@ -0,0 +1,12 @@
+package com.example.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    public String name;
+    public String password;
+    public int age;
+    public String mobile;
+
+}
--- src/test/java/com/example/chat/ChatApplicationTests.java
+++ src/test/java/com/example/chat/ChatApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.chat;
 
+import com.example.model.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,10 @@ class ChatApplicationTests {
 
     @Test
     void contextLoads() {
+        User user = new User();
+        user.setAge(11);
+        user.setMobile("asd");
+        System.out.println(user.getAge()+"!!!!!!!!!!!!!");
     }
 
 }
